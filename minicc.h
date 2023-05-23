655f512f912e680a9c13e2658ce5e3f1c13a0af5
cxt <3200104776@zju.edu.cn>
2023-05-23 10:22:28 +0800

    token类定义中增加了一些变量

--- minicc.h
+++ minicc.h
@@ -1,3 +1,24 @@
+#define _POSIX_C_SOURCE 200809L
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+typedef struct Token Token;
+struct Token {
+  TokenKind kind;   // Token kind
+  Token *next;      // Next token
+  int64_t val;      // If kind is TK_NUM, its value
+  char *loc;        // Token location
+  int len;          // Token length
+  Type *ty;         // Used if TK_NUM or TK_STR
+  char *str;        // String literal contents including terminating '\0'
+
+  File *file;       // Source location
+  char *filename;   // Filename
+  int line_no;      // Line number
+  int line_delta;   // Line number
+  bool at_bol;      // True if this token is at beginning of line
+  bool has_space;   // True if this token follows a space character
+  Hideset *hideset; // For macro expansion
+  Token *origin;    // If this is expanded from a macro, the original token
+};
